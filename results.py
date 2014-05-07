935abe9601674b965b0297ae1c9de1ebb7ab25c1
Diego Assencio <diego@assencio.com>
2014-05-07 13:40:58 +0200

    writes statistics to file, adds histogram functionality

--- results.py
+++ results.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-import math
+import numpy
 
 
 ##
@@ -26,24 +26,47 @@ def print_results(neighbors, words, out_file):
 ##
 # @brief analyzes and prints basic statistics about the results obtained
 # @param neighbors a list of lists containing the neighbors of each word
-# @param min_wlan smallest word length considered (smaller words are ignored)
+# @param min_wlen smallest word length considered (smaller words are ignored)
 # @param max_dist maximum considered distance between words
+# @param stats_file file where the statistics data will be written
 #
-def print_stats(neighbors, min_wlen, max_dist):
+def print_stats(neighbors, min_wlen, max_dist, stats_file):
 
 	nwords = len(neighbors)
 
-	counters = [len(x) for x in neighbors]
-	counters_sq = [x**2 for x in counters]
+	# counters contain the number of neighbors for each word
+	counters = numpy.array([len(x) for x in neighbors], dtype=numpy.int8)
 
-	# average number of neighbors
-	mean = float(sum(counters)) / float(nwords)
+	stats_file.write("Number of words analyzed: %d\n" % nwords)
+	stats_file.write("Smallest word length considered: %d\n" % min_wlen)
+	stats_file.write("Number of words within distance %d from any word\n" % max_dist)
+	stats_file.write("\tmean    : %f\n" % numpy.mean(counters))
+	stats_file.write("\tstd.dev.: %f\n\n" % numpy.std(counters))
 
-	# compute the standard deviation
-	std_dev = math.sqrt(sum(counters_sq)/float(nwords) - mean**2)
 
-	print "Smallest word length considered: %d" % min_wlen
-	print "Number of words analyzed: %d" % nwords
-	print "Number of words within distance %d from any word" % max_dist
-	print "\tmean    : %f" % mean
-	print "\tstd.dev.: %f" % std_dev
+#
+# @brief writes all histogram data (neighbor counter frequency) to a file
+# @param neighbors a list of lists containing the neighbors of each word
+# @param plot_hist True if the final histogram should be plotted, False otherwise
+# @param hist_file file on which the histogram data will be written
+#
+def print_histogram(neighbors, plot_hist, hist_file):
+
+	nwords = len(neighbors)
+
+	# counters contain the number of neighbors for each word
+	counters = numpy.array([len(x) for x in neighbors], dtype=numpy.int8)
+
+	hist, bins = numpy.histogram(counters, bins=max(counters)-min(counters), density=True)
+
+	for x,y in zip(bins,hist):
+		hist_file.write("%d %f\n" % (x,y))
+
+	if plot_hist:
+		import matplotlib.pyplot as pyplot
+
+		width = 0.7 * (bins[1] - bins[0])
+		center = (bins[:-1] + bins[1:]) / 2 - 0.5
+		pyplot.bar(center, hist, align='center', width=width)
+		pyplot.show()
+
