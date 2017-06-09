0b75b9b8168cebb3aff3176374205fd8b3ff8c9e
Diego Assencio <diego@assencio.com>
2017-06-09 21:10:13 +0200

    formats code

--- results.py
+++ results.py
@@ -7,76 +7,73 @@ import numpy
 
 
 def print_results(neighbors, words, out_file):
+    """
+    Prints the computed results to an output file.
 
-	"""
-	Prints the computed results to an output file.
+    neighbors - A list of lists containing the neighbors of each word.
+    words - The full list of words.
+    out_file - The file on which the results will be written.
+    """
 
-	neighbors - A list of lists containing the neighbors of each word.
-	words - The full list of words.
-	out_file - The file on which the results will be written.
-	"""
+    nwords = len(neighbors)
 
-	nwords = len(neighbors)
-
-	for i,neighbors_i in enumerate(neighbors):
-		out_file.write("%s:" % words[i])
-		for j in neighbors_i:
-			out_file.write(" %s" % words[j])
-		out_file.write("\n")
-	out_file.write("\n")
+    for i, neighbors_i in enumerate(neighbors):
+        out_file.write("%s:" % words[i])
+        for j in neighbors_i:
+            out_file.write(" %s" % words[j])
+        out_file.write("\n")
+    out_file.write("\n")
 
 
 def print_stats(neighbors, min_wlen, max_dist, stats_file):
+    """
+    Analyzes and prints basic statistics about the results obtained.
 
-	"""
-	Analyzes and prints basic statistics about the results obtained.
+    neighbors - A list of lists containing the neighbors of each word.
+    min_wlen - The smallest word length considered (smaller words are ignored).
+    max_dist - The maximum considered distance between words.
+    stats_file - The file on which the statistics data will be written.
+    """
 
-	neighbors - A list of lists containing the neighbors of each word.
-	min_wlen - The smallest word length considered (smaller words are ignored).
-	max_dist - The maximum considered distance between words.
-	stats_file - The file on which the statistics data will be written.
-	"""
+    nwords = len(neighbors)
 
-	nwords = len(neighbors)
+    # counters contain the number of neighbors for each word
+    counters = numpy.array([len(x) for x in neighbors], dtype=numpy.int8)
 
-	# counters contain the number of neighbors for each word
-	counters = numpy.array([len(x) for x in neighbors], dtype=numpy.int8)
-
-	stats_file.write("Number of words analyzed: %d\n" % nwords)
-	stats_file.write("Smallest word length considered: %d\n" % min_wlen)
-	stats_file.write("Number of words within distance %d from any word\n" % max_dist)
-	stats_file.write("\tmean    : %f\n" % numpy.mean(counters))
-	stats_file.write("\tstd.dev.: %f\n\n" % numpy.std(counters))
+    stats_file.write("Number of words analyzed: %d\n" % nwords)
+    stats_file.write("Smallest word length considered: %d\n" % min_wlen)
+    stats_file.write(
+        "Number of words within distance %d from any word\n" % max_dist)
+    stats_file.write("\tmean    : %f\n" % numpy.mean(counters))
+    stats_file.write("\tstd.dev.: %f\n\n" % numpy.std(counters))
 
 
 def print_histogram(neighbors, plot_hist, hist_file):
+    """
+    Writes all histogram data (neighbor counter frequency) to a file.
 
-	"""
-	Writes all histogram data (neighbor counter frequency) to a file.
+    neighbors - A list of lists containing the neighbors of each word.
+    plot_hist - True if the final histogram should be plotted, False otherwise.
+    hist_file - The file on which the histogram data will be written.
+    """
 
-	neighbors - A list of lists containing the neighbors of each word.
-	plot_hist - True if the final histogram should be plotted, False otherwise.
-	hist_file - The file on which the histogram data will be written.
-	"""
+    nwords = len(neighbors)
 
-	nwords = len(neighbors)
+    # counters contain the number of neighbors for each word
+    counters = numpy.array([len(x) for x in neighbors], dtype=numpy.int8)
 
-	# counters contain the number of neighbors for each word
-	counters = numpy.array([len(x) for x in neighbors], dtype=numpy.int8)
+    # we need at least one bin (otherwise an exception will be thrown)
+    nbins = max(1, max(counters) - min(counters))
 
-	# we need at least one bin (otherwise an exception will be thrown)
-	nbins = max(1, max(counters)-min(counters))
+    hist, bins = numpy.histogram(counters, bins=nbins, density=True)
 
-	hist, bins = numpy.histogram(counters, bins=nbins, density=True)
+    for x, y in zip(bins, hist):
+        hist_file.write("%d %f\n" % (x, y))
 
-	for x,y in zip(bins,hist):
-		hist_file.write("%d %f\n" % (x,y))
+    if plot_hist:
+        import matplotlib.pyplot as pyplot
 
-	if plot_hist:
-		import matplotlib.pyplot as pyplot
-
-		width = 0.7 * (bins[1] - bins[0])
-		center = (bins[:-1] + bins[1:]) / 2 - 0.5
-		pyplot.bar(center, hist, align='center', width=width)
-		pyplot.show()
-
+        width = 0.7 * (bins[1] - bins[0])
+        center = (bins[:-1] + bins[1:]) / 2 - 0.5
+        pyplot.bar(center, hist, align="center", width=width)
+        pyplot.show()
--- workers.py
+++ workers.py
@@ -9,49 +9,49 @@ import time
 
 
 def compute_distances(distmap, words, i_min, i_max, max_dist, counter):
+    """
+    Computes distances between words.
 
-	"""
-	Computes distances between words.
+    distmap - A dictionary (i,j) --> dist(i,j) where i and j are word indices.
+    words - The full list of words.
+    i_min/i_max - indices defining the words for which distances will be computed.
+    max_dist - The maximum considered distance between words.
+    counter - A global counter which will hold the number of words processed.
+    """
 
-	distmap - A dictionary (i,j) --> dist(i,j) where i and j are word indices.
-	words - The full list of words.
-	i_min/i_max - indices defining the words for which distances will be computed.
-	max_dist - The maximum considered distance between words.
-	counter - A global counter which will hold the number of words processed.
-	"""
-
-	for i in range(i_min, i_max):
-		for j in range(i+1, len(words)):
-			if abs(len(words[i]) - len(words[j])) <= max_dist:
-				d_ij = strdist.levenshtein(words[i],words[j])
-				if d_ij <= max_dist:
-					distmap[i,j] = d_ij
-					distmap[j,i] = d_ij
-		counter.value += 1
+    for i in range(i_min, i_max):
+        for j in range(i + 1, len(words)):
+            if abs(len(words[i]) - len(words[j])) <= max_dist:
+                d_ij = strdist.levenshtein(words[i], words[j])
+                if d_ij <= max_dist:
+                    distmap[i, j] = d_ij
+                    distmap[j, i] = d_ij
+        counter.value += 1
 
 
 def show_progress(counter, nwords):
+    """
+    Shows the progress of the computing subprocesses.
 
-	"""
-	Shows the progress of the computing subprocesses.
+    counter - The global number of words processed so far.
+    nwords - The total number of words which must be processed.
+    """
 
-	counter - The global number of words processed so far.
-	nwords - The total number of words which must be processed.
-	"""
-
-	start = time.time()
-	prev_progress = 0.0
-	while True:
-		progress = float(counter.value) / float(nwords)
-		if progress - prev_progress >= 0.0001:
-			now = time.time()
-			# etc: estimated time to completion (in seconds)
-			etc = (now - start) * (1 - progress) / progress
-			etc_m, etc_s = divmod(etc, 60)
-			etc_h, etc_m = divmod(etc_m, 60)
-			sys.stderr.write("progress: %6.2f%%   time left: %02d:%02d:%02d\n" % (
-						100*progress, etc_h, etc_m, etc_s
-					))
-			prev_progress = progress
-			if counter.value == nwords: break
-			time.sleep(1)
+    start = time.time()
+    prev_progress = 0.0
+    while True:
+        progress = float(counter.value) / float(nwords)
+        if progress - prev_progress >= 0.0001:
+            now = time.time()
+            # etc: estimated time to completion (in seconds)
+            etc = (now - start) * (1 - progress) / progress
+            etc_m, etc_s = divmod(etc, 60)
+            etc_h, etc_m = divmod(etc_m, 60)
+            sys.stderr.write(
+                "progress: %6.2f%%   time left: %02d:%02d:%02d\n" % (
+                    100 * progress, etc_h, etc_m, etc_s
+                ))
+            prev_progress = progress
+            if counter.value == nwords:
+                break
+            time.sleep(1)
